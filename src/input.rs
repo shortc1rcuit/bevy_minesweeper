d30d3fb3bb4f9b159bfc68f1d9d9d3efd11cb39d
shortc1rcuit <declan@themcmoos.net>
2023-03-17 21:59:02 +0000

    No wait I did need that reasource

--- src/input.rs
+++ src/input.rs
@@ -1,8 +1,53 @@
 use crate::board::board_gen::TileType;
 use crate::board::{GameBoard, TileEntity};
-use crate::{HEIGHT, WIDTH};
+use crate::MainCamera;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+
+/// Resource that contains the location of the cursor in world space.
+#[derive(Reflect, Resource, Deref, DerefMut, Default)]
+#[reflect(Resource)]
+struct CursorWorldPos(Option<Vec2>);
+
+//Taken from https://bevy-cheatbook.github.io/cookbook/cursor2world.html#2d-games
+/// Converts the location of the cursor on the screen to the location in the world.
+fn cursor_to_world_pos(
+    // need to get window dimensions
+    wnds: Query<&Window, With<PrimaryWindow>>,
+    // query to get camera transform
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut cursor_world_pos: ResMut<CursorWorldPos>,
+) {
+    // get the camera info and transform
+    // assuming there is exactly one main camera entity, so query::single() is OK
+    let (camera, camera_transform) = q_camera.single();
+
+    let Ok(wnd) = wnds.get_single() else {
+        return;
+    };
+
+    // check if the cursor is inside the window and get its position
+    if let Some(screen_pos) = wnd.cursor_position() {
+        // get the size of the window
+        let window_size = Vec2::new(wnd.width(), wnd.height());
+
+        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+        let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+
+        // matrix for undoing the projection and camera transform
+        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+
+        // use it to convert ndc to world-space coordinates
+        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+
+        // reduce it to a 2D value
+        let world_pos: Vec2 = world_pos.truncate();
+
+        cursor_world_pos.0 = Some(world_pos);
+    } else {
+        cursor_world_pos.0 = None;
+    }
+}
 
 /// Struct that defines a bounding box.
 #[derive(Reflect, Default)]
@@ -66,15 +111,10 @@ impl Selectable {
 /// Sets the selection types of any Selectable entities based on the cursor position and mouse button state.
 fn set_selection(
     mut selectables: Query<&mut Selectable>,
-    primary_window: Query<&Window, With<PrimaryWindow>>,
+    cursor_pos: Res<CursorWorldPos>,
     mouse_input: Res<Input<MouseButton>>,
 ) {
-    let Ok(window) = primary_window.get_single() else {
-        return;
-    };
-
-    let Some(position) = window.cursor_position() else { return };
-    let position = position - Vec2::new(WIDTH / 2., HEIGHT / 2.);
+    let Some(position) = cursor_pos.0 else { return };
 
     let interaction;
 
@@ -114,6 +154,7 @@ pub struct MyInputPlugin;
 impl Plugin for MyInputPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Selectable>()
-            .add_systems((set_selection, tile_click).chain());
+            .init_resource::<CursorWorldPos>()
+            .add_systems((cursor_to_world_pos, set_selection, tile_click).chain());
     }
 }
